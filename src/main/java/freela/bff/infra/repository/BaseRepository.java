95d78e86bcf7b9ef1b633ac2b81283f2d5593cc6
Paulo-Enrique-Santos <paulo.santosfo@sptech.school>
2023-10-24 21:22:07 -0300

    feat: ajuste do client

--- src/main/java/freela/bff/infra/repository/BaseRepository.java
+++ src/main/java/freela/bff/infra/repository/BaseRepository.java
@@ -4,10 +4,7 @@ import com.google.gson.Gson;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -61,6 +58,20 @@ public class BaseRepository {
         }
     }
 
+    public <T> T sendPatch(HttpPatch uri, Class<T> responseType) {
+        try {
+            HttpResponse response = this.client.execute(uri);
+            String responseBody = EntityUtils.toString(response.getEntity());
+
+            Gson gson = new Gson();
+            T responseObj = gson.fromJson(responseBody, responseType);
+
+            return responseObj;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public <T> T sendDelete(HttpDelete uri, Class<T> responseType) {
         try {
             HttpResponse response = this.client.execute(uri);
@@ -75,10 +86,31 @@ public class BaseRepository {
         }
     }
 
-    public HttpPost generateBody(HttpPost post, String json){
+    public HttpPost generateBody(HttpPost post, Object objeto){
+        String json = this.convertObjectToJson(objeto);
         HttpEntity entity = new StringEntity(json,"UTF-8");
+        post.setHeader("Content-Type", "application/json");
         post.setEntity(entity);
 
         return post;
+    }
+
+    public HttpPatch generateBody(HttpPatch post, Object objeto){
+        String json = this.convertObjectToJson(objeto);
+        HttpEntity entity = new StringEntity(json,"UTF-8");
+        post.setHeader("Content-Type", "application/json");
+        post.setEntity(entity);
+
+        return post;
+    }
+
+    public String convertObjectToJson(Object object) {
+        try {
+            Gson gson = new Gson();
+            return gson.toJson(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
